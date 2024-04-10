d6bbc84d0f2e398db7568f0218dad5d3ec270b55
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-04-10 16:56:30 -0300

    Add files via upload

---
+++ Att01/BusAtt01.js
@@ -0,0 +1,22 @@
+const data = require('./onibus.json')
+console.log(data)
+
+
+
+//Crie uma função que aceite o ID da linha como entrada e retorne os horários de partida e chegada para essa linha.
+
+const idLinha = (data) => {
+    data.forEach((linhas)=>{
+        if (linhas === linhas[i]) {
+            console.log(`O ID da Linha é: ${linhas[i]}`) 
+        } else {
+            console.log(console.error('erro'))
+        }
+    // console.log(data.linhas[0].id)
+    // console.log(data.linhas[0].horarios)
+    // console.log(data.linhas[1].id)
+    // console.log(data.linhas[1].horarios)
+    return 
+     })
+}
+console.log(idLinha(data))
